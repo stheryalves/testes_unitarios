ade0c30db3e38f14fa26e4eb40e7cee76f5713ba
Sthery <sthery.styll@gmail.com>
2024-06-11 11:50:01 +0100

    teste suite / asserções/matches

--- src/libs/__tests__/sum.spect.ts
+++ src/libs/__tests__/sum.spect.ts
@@ -1,7 +1,9 @@
 import { sum } from "../sum";
 
 describe('libs suite', () => {
-    it('deve somar 1 + 2 e receber 3 como resposta', () => {
-        expect(sum(1, 2)).toBe(3);
-    })
+    describe('sum suite', () => {
+        it('deve somar 1 + 2 e receber 3 como resposta', () => {
+            expect(sum(1, 2)).toBe(3);
+        })
+    })    
 })
